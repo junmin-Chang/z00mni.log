b3a34959decfb0902739f3554664b596acfc0b66
junmin-chang <chjm219@kookmin.ac.kr>
2021-05-11 02:05:01 +0900

    home 화면 수정

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,8 +4,6 @@ function Home() {
     return (
         <div className="container">
             <h1>장준민의 개발 블로그에 오신 것을 환영합니다.</h1>
-
-            <p>저는 풀스택 개발자를 목표로 하고 있는 21살 대학생 입니다. 전자공학을 전공하고 있고, ReactJS, NodeJS, express, MongoDB를 다룰 수 있습니다. </p>
          
         </div>
 
