aae955b025ee5bb82f02c71b41479dd8ff6b9219
junmin-chang <chjm219@kookmin.ac.kr>
2021-05-11 21:50:22 +0900

    patch method issue-1

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,15 @@ require('dotenv').config();
 
 
 const app = express();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 app.use(cors());
+
 app.use(express.json());
 
 const PORT = process.env.PORT || 80;
