b9f3f1fc214faa091258868740a1578094027758
junmin-chang <chjm219@kookmin.ac.kr>
2021-05-12 13:04:34 +0900

    urlencoded-2

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
