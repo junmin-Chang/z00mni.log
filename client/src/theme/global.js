a51ef2875233f27df5da49539dbe07a51a3f4b9d
junmin-chang <chjm219@kookmin.ac.kr>
2021-05-14 17:00:25 +0900

    navbar 이슈 해결

--- client/src/theme/global.js
+++ client/src/theme/global.js
@@ -12,11 +12,20 @@ export const GlobalStyles = createGlobalStyle`
     color: ${({ theme }) => theme.text};
     font-family: BlinkMacSystemFont, -apple-system, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif;
     transition: all 0.25s linear;
+    
   }
   .navbar {
     background: ${({ theme }) => theme.navBody};
     color: ${({ theme }) => theme.navText};
     transition: all 0.25s linear;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    height: 50px;
+    overflow: hidden;
+    z-index: 1;
+    box-shadow: 0 2px 4px rgba(0,0,0,0.2);
+ 
   }
   .navbar .links a {
     color: ${({ theme }) => theme.navText};
