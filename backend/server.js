3d76dcf15406edbafa4e677996fb8f629f71c0ad
junmin-chang <chjm219@kookmin.ac.kr>
2021-05-17 16:47:04 +0900

    heroku cors

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,7 @@ require('dotenv').config();
 
 
 const app = express();
-app.use(cors());
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cookieParser);
 
 
 const PORT = process.env.PORT || 80;
@@ -24,6 +20,22 @@ console.log('Server is Starting...');
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
 })
+console.log('Connecting to Mongo db');
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+    }, (err) => {
+    if (err) return console.log(err.message);
+
+    console.log('MongoDB connection established');
+})
+app.use(cors());
+app.use(cookieParser);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use('/posts', require('./routes/postRoutes'));
 app.use('/', require('./routes/writeRoutes'))
@@ -72,15 +84,5 @@ app.get('/auth', auth, (req,res) => {
 })
 
 
-console.log('Connecting to Mongo db');
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-    }, (err) => {
-    if (err) return console.log(err.message);
 
-    console.log('MongoDB connection established');
-})
 
