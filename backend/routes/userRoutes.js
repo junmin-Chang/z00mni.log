1f31924038c136742ef06fc3e3eb65f11ec1d12c
junmin-chang <chjm219@kookmin.ac.kr>
2021-05-17 15:57:17 +0900

    userRoutes define

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const { User } = require('../model/userModel');
+const { auth } = require('./auth');
+const router = express.Router();
+
+//register
+router.post('/register', (req,res) => {
+    const user = new User(req.body);
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err});
+        return res.status(200).json({ success: true });
+    })
+})
+
+//login
+router.post('/login', (req,res) => {
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: '요청된 이메일에 해당하는 유저가 없음.'
+            })
+        }
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch) 
+                return res.json({ loginSuccess: false , message: '비밀번호가 서로 다름.'})
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
+                res.cookie("x_auth", user.token)
+                    .status(200)
+                    .json({ loginSuccess: true, useId: user._id})
+            })
+        })
+    })
+})
+
+router.get('/auth', auth, (req,res) => {
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false: true,
+        isAuth: true,
+        email: req.body.email,
+        name: req.user.name,
+        role: req.user.role
+    })
+})
