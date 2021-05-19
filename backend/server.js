4820b05f59d3deae09901cacc3cd39de13cb1b43
junmin-chang <chjm219@kookmin.ac.kr>
2021-05-19 19:35:23 +0900

    origin

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ mongoose.connect(process.env.MONGODB_URI, {
 
     console.log('MongoDB connection established');
 })
-app.use(cors({ origin: 'https://zoomni-log.herokuapp.com/', credentials: true }));
+app.use(cors({ credentials: true }));
 app.use(cookieParser())
 
 
