142fde959d6fc52c82406151d6faefd7c44c7309
junmin-chang <chjm219@kookmin.ac.kr>
2021-05-19 22:35:33 +0900

    user null

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -11,6 +11,7 @@ let auth = (req, res, next) => {
     User.findByToken(token, (err, user) => {
         console.log('decoded token', token);
         if (err) throw err;
+        if (!user) return res.json({ isAuth: false, error: true});
         req.token = token;
         req.user = user;
         next();
