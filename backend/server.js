f12466b354a6ce8b5bb4ea30d34a7226f887ace4
junmin-chang <chjm219@kookmin.ac.kr>
2021-05-19 19:01:19 +0900

    cookie domain

--- backend/server.js
+++ backend/server.js
@@ -77,7 +77,7 @@ app.post('/login', (req,res) => {
             user.generateToken((err, user) => {
                 if (err) return res.status(400).send(err);
                 console.log("로그인 시 토큰:", user.token)
-                res.cookie("x_auth", user.token, { httpOnly: false })
+                res.cookie("x_auth", user.token, { httpOnly: false, domain:'https://z00mni-log.netlify.app/' })
                     .status(200)
                     .json({ loginSuccess: true, useId: user._id})
                     console.log(req.cookies.x_auth);
