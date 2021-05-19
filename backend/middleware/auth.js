e8e28f38fdc7fec6bb65992b747159572bcae9da
junmin-chang <chjm219@kookmin.ac.kr>
2021-05-19 22:25:45 +0900

    error

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -11,7 +11,6 @@ let auth = (req, res, next) => {
     User.findByToken(token, (err, user) => {
         console.log('decoded token', token);
         if (err) throw err;
-        if (!user) return res.json({ isAuth: false, error: true});
         req.token = token;
         req.user = user;
         next();
