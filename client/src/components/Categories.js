fa1ee90d50d7e5097b4243cf85d99158560a6ea1
junmin-chang <chjm219@kookmin.ac.kr>
2021-05-28 17:59:54 +0900

    categories issue fix

--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -9,7 +9,7 @@ function Categories() {
         <div className="tag-wrapper">
         <h3>#카테고리</h3>
         <span className="tag-all" onClick={() => dispatch(getPosts())}>전체</span>
-        <span className="tags" onClick={() => dispatch(getPostsByTag("리액트"))}>#React</span>
+        <span className="tags" onClick={() => dispatch(getPostsByTag("React"))}>#React</span>
         <span className="tags" onClick={() => dispatch(getPostsByTag("Redux"))}>#Redux</span>
         <span className="tags" onClick={() => dispatch(getPostsByTag("NodeJS"))}>#NodeJS</span>
         <span className="tags" onClick={() => dispatch(getPostsByTag("삽질"))}>#삽질</span>
