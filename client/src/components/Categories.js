46472330c0d7be09bc3277c87f2262cbe5eb6a5c
junmin-chang <chjm219@kookmin.ac.kr>
2021-06-27 20:03:14 +0900

    add category of GraphQL

--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -10,6 +10,7 @@ function Categories() {
         <span className="tag-all" onClick={() => dispatch(getPosts())}>전체</span>
         <span className="tags" onClick={() => dispatch(getPostsByTag("React"))}>#React</span>
         <span className="tags" onClick={() => dispatch(getPostsByTag("Redux"))}>#Redux</span>
+        <span className="tags" onClick={() => dispatch(getPostsByTag("GraphQL"))}>#GraphQL</span>
         <span className="tags" onClick={() => dispatch(getPostsByTag("NodeJS"))}>#NodeJS</span>
         <span className="tags" onClick={() => dispatch(getPostsByTag("삽질"))}>#삽질</span>
     </div>
