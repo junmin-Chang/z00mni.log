c6f6126380928963795766c50f3beedd35ffcc8d
junmin-chang <chjm219@kookmin.ac.kr>
2021-07-01 15:33:07 +0900

    convert to styled-components-5 (modal)

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -1,23 +1,81 @@
 import React from 'react';
+import { ModalWrapper } from '../style/Wrapper';
+import styled, { keyframes } from 'styled-components'
+
+const show = keyframes`
+    from {
+    opacity: 0;
+    margin-top: -50px;
+  }
+    to {
+    opacity: 1;
+    margin-top: 0;
+  }
+`
+const ModalSection = styled.section`
+    width: 90%;
+    max-width: 450px;
+    margin: 0 auto;
+    border-radius: 0.3rem;
+    background-color: #fff;
+  /* 팝업이 열릴때 스르륵 열리는 효과 */
+    animation: ${show} 0.3s;
+    overflow: hidden;
+`
+const ModalHeader = styled.header`
+    position: relative;
+    padding: 16px 64px 16px 16px;
+    background-color: #f1f1f1;
+    font-weight: 700;
+`
+
+const ModalMain = styled.main`
+    padding: 16px;
+    border-bottom: 1px solid #dee2e6;
+    border-top: 1px solid #dee2e6;
+`
+const ModalFooter = styled.footer`
+    padding: 12px 16px;
+    text-align: right;
+`
+const HeaderButton = styled.button`
+    position: absolute;
+    top: 15px;
+    right: 15px;
+    width: 30px;
+    font-size: 21px;
+    font-weight: 700;
+    text-align: center;
+    color: #999;
+    background-color: transparent;
+`
+
+const FooterButton = styled.button`
+    padding: 6px 12px;
+    color: #fff;
+    background-color: #6c757d;
+    border-radius: 5px;
+    font-size: 13px;
+`
 function Modal(props) {
 
     return (
-        <div className={ props.open ? 'openModal modal' : 'modal' }>
+       <ModalWrapper open={props.open}>
         { props.open ? (  
-            <section>
-                <header>
+            <ModalSection>
+                <ModalHeader>
                     {props.header}
-                    <button className="close" onClick={props.close}> &times; </button>
-                </header>
-                <main>
+                    <HeaderButton onClick={props.close}> &times; </HeaderButton>
+                </ModalHeader>
+                <ModalMain>
                     {props.children}
-                </main>
-                <footer>
-                    <button className="close" onClick={props.close}> 닫기 </button>
-                </footer>
-            </section>
+                </ModalMain>
+                <ModalFooter>
+                    <FooterButton onClick={props.close}> 닫기 </FooterButton>
+                </ModalFooter>
+            </ModalSection>
         ) : null }
-    </div>
+        </ModalWrapper>
 
     )
     
--- client/src/components/style/Wrapper.js
+++ client/src/components/style/Wrapper.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { css, keyframes } from 'styled-components'
 
 export const Wrapper = styled.div`
     max-width: 800px;
@@ -12,4 +12,25 @@ export const NavWrapper = styled.div`
     padding: 0 1rem;
     align-items: center;
     justify-content: space-evenly;
+`
+
+const bgShow = keyframes`
+    from {
+    opacity: 0;
+    }
+    to {
+    opacity: 1;
+    }
+`
+export const ModalWrapper= styled.div`
+    display: ${props => props.open ? "flex" : "none"};
+    align-items: ${props => props.open ? "center" : "none"};
+    animation: ${props => props.open ? css`${bgShow} 0.3s` : "none"};
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 99;
+    background-color: rgba(0, 0, 0, 0.6);
 `
